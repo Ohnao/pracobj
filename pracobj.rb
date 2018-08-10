b588284a12cb116855a6bca5dc09334f7a1ed7c5
naoya.ohsaki <noblesse.oblige7@suou.waseda.jp>
2018-08-10 17:49:20 +0900

    first commit

---
+++ pracobj.rb
@@ -0,0 +1,14 @@
+class human
+  def initialize(name,tall)
+    @name = name
+    @tall = tall
+  end
+
+  def eating
+    puts 'I am eating'
+  end
+
+  def making_me_sleep
+    puts 'I am sleeping'
+  end
+end
