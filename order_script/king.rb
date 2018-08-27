129848187b319a11b0e05d3390e64fe6a94ae447
naoya.ohsaki <noblesse.oblige7@suou.waseda.jp>
2018-08-27 20:52:42 +0900

    クラスを継承させました

--- order_script/king.rb
+++ order_script/king.rb
@@ -1,3 +1,12 @@
 require_relative 'boss'
+require_relative 'extra_class'
+require_relative 'sub_boss'
+require_relative 'subordinate'
 
+chinko = Extraclass.new
+boss2 = Subboss.new
+boss3 = Subordinate.new
 
+chinko.standup
+boss2.standup
+boss3.standup
