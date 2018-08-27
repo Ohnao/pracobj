756e7b60c1b912e1350f831d18be28c545c96615
naoya.ohsaki <noblesse.oblige7@suou.waseda.jp>
2018-08-27 21:00:05 +0900

    忘れないようにコメントを追加しておきました

--- order_script/king.rb
+++ order_script/king.rb
@@ -1,12 +1,15 @@
+## 参照するファイル群
 require_relative 'boss'
 require_relative 'extra_class'
 require_relative 'sub_boss'
 require_relative 'subordinate'
 
+## 変数を任意で用意し、クラスをnewメソッドにより、オブジェクトとして生成する。これにより、参照先のメソッドが使えるようになる
 chinko = Extraclass.new
 boss2 = Subboss.new
 boss3 = Subordinate.new
 
+## メソッドの中身は、sub_boss,extra_class,subordinate,それぞれのファイルごとに別のものを用意してあるので、クラスを継承させたファイルは同じ名前のメソッドを持っていても、その挙動は変化する。
 chinko.standup
 boss2.standup
 boss3.standup
