e794e211bc93ccc1796765164ac492af173f7a20
naoya.ohsaki <noblesse.oblige7@suou.waseda.jp>
2018-08-30 21:15:27 +0900

    コメントに加筆をしました

--- order_script/king.rb
+++ order_script/king.rb
@@ -9,7 +9,7 @@ chinko = Extraclass.new
 boss2 = Subboss.new
 boss3 = Subordinate.new
 
-## メソッドの中身は、sub_boss,extra_class,subordinate,それぞれのファイルごとに別のものを用意してあるので、クラスを継承させたファイルは同じ名前のメソッドを持っていても、その挙動は変化する。この挙動だけが変化している差を生み出せることがポリモーフィズムという
+## メソッドの中身は、sub_boss,extra_class,subordinate,それぞれのファイルごとに別のものを用意してあるので、クラスを継承させたファイルは同じ名前のメソッドを持っている。しかし、その挙動は変化する。(現に出力結果は異なっている)この挙動だけが変化している差を生み出せることがポリモーフィズムという
 chinko.standup
 boss2.standup
 boss3.standup
