e3fcfe35b01e07e4547aff977b58c605fd4a81fd
naoya.ohsaki <noblesse.oblige7@suou.waseda.jp>
2018-09-03 12:28:53 +0900

    変数名をわかりやすい名前に変更しました

--- order_script/king.rb
+++ order_script/king.rb
@@ -5,15 +5,15 @@ require_relative 'sub_boss'
 require_relative 'subordinate'
 
 ## 変数を任意で用意し、クラスをnewメソッドにより、オブジェクトとして生成する。これにより、参照先のメソッドが使えるようになる
-chinko = Extraclass.new
-boss2 = Subboss.new
-boss3 = Subordinate.new
+extra_class = Extraclass.new
+sub_boss = Subboss.new
+subordinate = Subordinate.new
 
 ## メソッドの中身は、sub_boss,extra_class,subordinate,それぞれのファイルごとに別のものを用意してあるので、クラスを継承させたファイルは同じ名前のメソッドを持っている。しかし、その挙動は変化する。(現に出力結果は異なっている)この挙動だけが変化している差を生み出せることがポリモーフィズムという
-chinko.standup
-boss2.standup
-boss3.standup
+extra_class.standup
+sub_boss.standup
+subordinate.standup
 
-puts"私の給料は、#{chinko.nomal_salary(100)}円です"
-puts"私の給料は、#{boss2.nomal_salary(100)}円です"
-puts"私の給料は、#{boss3.nomal_salary(100)}円です"
+puts"私の給料は、#{extra_class.nomal_salary(100)}円です"
+puts"私の給料は、#{sub_boss.nomal_salary(100)}円です"
+puts"私の給料は、#{subordinate.nomal_salary(100)}円です"
